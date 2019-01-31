a49f5293105c96563d5a301c23b34d0ae98ffd7d
Александр Питасов <alexpit63@gmail.com>
2019-01-31 22:30:36 +0300

    1st lesson after mustache refactoring в методе greeting маппируется раздел сайта /greering  с параметром name?="some string value" в методе main маппируется корневой раздел сайта
    в обоих методах в параметрах объявляется модель - коллекция "строка-объект", в теле метода закладывается один параметр, затем выводится вьюха - темплейт из папки темлейт
    
    пока все понятно, можно даже попробовать написать свой веб-сервис

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -5,20 +5,46 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    /*Аннотация @RequestMapping гарантирует, что HTTP запросы к
+    /*Аннотация @<Request>Mapping гарантирует, что HTTP запросы к
     /greeting приведут к выполнению метода greeting(). */
-    public String greeting (@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting (@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+        //В МУСТАШЕ ФРЕЙМВОРКЕ ШАБЛОНОВ ИСПОЛЬЗУЕТСЯ Map<Ключ, значение>
         /*@RequestParam связывает значение строкового параметра name запроса с name параметром метода greeting().
         Этот параметр не required; если он отсутствует в запросе, то будет использовано defaultValue значение "World".
         Значение параметра name добавлено в объект Model, что делает его доступным в шаблоне представления.*/
-        model.addAttribute("name", name);
-        return "greeting";
+        model.put("name", name);   //т.е. добавляем к модели представления (возвращаемой ниже) атрибут name, описанный выше
+        return "greeting";      //возвращаем файл шаблона greeting из папки templates
     }
     /*В примере выше не определено GET, PUT, POST и так далее, потому что
     @RequestMapping соответствует всем HTTP операциям по умолчанию.
     @RequestMapping(method=GET) уточняет это соответствие.*/
+
+    @GetMapping     //GET-метод главной страницы сайта
+    public String main(Map<String, Object> model){
+        model.put("some", "Hello, letscode!");
+        return "main";
+    }
 }
+/*  СТАРЫЙ МЕТОД, КОТОРЫЙ БЫЛ НАПИСАН ПОД ТАЙМЛИФ
+    @GetMapping("/greeting")
+    /*Аннотация @<Request>Mapping гарантирует, что HTTP запросы к
+    /greeting приведут к выполнению метода greeting().
+    public String greeting (@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+
+        model.addAttribute("name", name);   //т.е. добавляем к модели представления (возвращаемой ниже) атрибут name, описанный выше
+        return "greeting";      //возвращаем файл шаблона greeting из папки templates
+    }*/
+
+    /*@RequestParam связывает значение строкового параметра name запроса с name параметром метода greeting().
+        Этот параметр не required; если он отсутствует в запросе, то будет использовано defaultValue значение "World".
+        Значение параметра name добавлено в объект Model, что делает его доступным в шаблоне представления.
+
+    /*В примере выше не определено GET, PUT, POST и так далее, потому что
+    @RequestMapping соответствует всем HTTP операциям по умолчанию.
+    @RequestMapping(method=GET) уточняет это соответствие.*/
